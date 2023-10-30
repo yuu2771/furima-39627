81872648a7d48d89a1438539ee24b3735cc06f9d
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-10-30 14:40:00 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_furigana, presence: true
+  validates :first_name_furigana, presence: true
+  validates :birthday, presence: true
+  
 end
