8eaf3209d43370e15071626428c3689aa9e136cd
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-10-31 09:00:43 +0900

    バリデーション設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_furigana, presence: true
