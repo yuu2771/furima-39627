59743a3139b508a5e79befb37be59e73c5830856
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-01 10:55:49 +0900

    userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_furigana, presence: true
-  validates :first_name_furigana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_furigana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
+  validates :first_name_furigana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
   validates :birthday, presence: true
-  
+
 end
