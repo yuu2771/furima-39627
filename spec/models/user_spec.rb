45f2f167dcf8e3c99a0e42b76ec5574c746b9888
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-01 15:36:30 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,9 +46,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは半角英数字が入ってないと登録できない' do
+      it 'passwordは半角英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordは半角数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordは全角では登録できない' do
+        @user.password = 'ａａａ０００'
+        @user.password_confirmation = 'ａａａ０００'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is invalid")
       end
