fb20f7cf844aed6140fce4409155ddec4c8f8eb8
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-09 21:34:15 +0900

    リファクタリングの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,8 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to action: :index
     end
   end
