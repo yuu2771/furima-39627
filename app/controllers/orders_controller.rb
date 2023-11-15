6db9c0794e6275467325d2562de5a9c442d3557e
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-15 14:07:01 +0900

    一部修正と商品表示のための実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+  end
+
+  private
+
+
+
+
+end
--- spec/requests/oders_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Oders", type: :request do
-
-end
