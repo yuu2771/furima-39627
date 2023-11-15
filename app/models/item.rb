538b49fdc33e2c87ad880cdb53872c3e0b5943bb
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-15 10:17:48 +0900

    モデルの作成とアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   validates :image, :title, :description, presence: true
   validates :category_id, :condition_id, :prefecture_id, :shipping_charge_id, :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,6 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   has_many :items
+  has_many :orders
 
 end
