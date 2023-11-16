019242c49daf0ae0844acb3cddc382237892831f
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-16 10:04:01 +0900

    createアクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,11 +6,21 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
-
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
 
 end
