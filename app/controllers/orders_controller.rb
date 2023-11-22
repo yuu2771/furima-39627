de864e2ceaf4cca3d4204ca5b2aa74faace6a589
yuu2771 <141595992+yuu2771@users.noreply.github.com>
2023-11-22 15:04:46 +0900

    一部リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :set_public_key
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @orders_with_item_id = Order.where(item_id: @item.id)
     unless user_signed_in?
       redirect_to new_user_session_path
@@ -15,7 +15,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -26,6 +25,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture, :municipalities, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
